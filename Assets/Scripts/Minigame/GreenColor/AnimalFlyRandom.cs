87e8543711a8ca4e482a1ca97ac06d9b378f35e0
Pham Duc Toan <toanphamduc.411@gmailc.om>
2024-04-12 21:11:50 +0700

    update cho ếch ăn

--- Assets/Scripts/Minigame/GreenColor/AnimalFlyRandom.cs
+++ Assets/Scripts/Minigame/GreenColor/AnimalFlyRandom.cs
@@ -1,21 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using Spine.Unity;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimalFlyRandom : MonoBehaviour
 {
+    public bool isCurrentAnimal;
+
     public RectTransform areaRect;
 
     public float speedFly;
 
     private bool isFly = true;
 
+    public Action actionClick {  get; set; }
+
     private void Start()
     {
         isFly = true;
         MoveRandom();
+        GetComponent<Button>().onClick.AddListener(ClickAnimal);
     }
 
     public void CancelFly()
@@ -24,13 +31,18 @@ public class AnimalFlyRandom : MonoBehaviour
         isFly = false;
     }
 
+    private void ClickAnimal()
+    {
+        actionClick?.Invoke();
+    }
+
     private Vector2 GetRandomPositionInUI(RectTransform areaRect)
     {
         Vector2 min = areaRect.rect.min;
         Vector2 max = areaRect.rect.max;
 
-        float randomX = Random.Range(min.x, max.x);
-        float randomY = Random.Range(min.y, max.y);
+        float randomX = UnityEngine.Random.Range(min.x, max.x);
+        float randomY = UnityEngine.Random.Range(min.y, max.y);
 
         return new Vector2(randomX, randomY);
     }
@@ -40,16 +52,17 @@ public class AnimalFlyRandom : MonoBehaviour
         if (!isFly)
             return;
         Vector2 randomPosition = GetRandomPositionInUI(areaRect);
-        if(randomPosition.x - transform.position.x > 0)
+        if(randomPosition.x - transform.localPosition.x > 0)
         {
-            GetComponent<SkeletonGraphic>().initialFlipX = true;
+            transform.localScale = new Vector2(-Math.Abs(transform.localScale.x), transform.localScale.y);
         }
         else
         {
-            GetComponent<SkeletonGraphic>().initialFlipX = false;
+            transform.localScale = new Vector2(Math.Abs(transform.localScale.x), transform.localScale.y);
         }
 
-        float dis = Vector2.Distance(transform.position, randomPosition);
+        float dis = Vector2.Distance(transform.localPosition, randomPosition);
+        //transform.rotation = Vector2Extensions.LookAt2D(transform.position, randomPosition);
         transform.DOLocalMove(randomPosition, dis / speedFly).OnComplete(() => {
             MoveRandom();
         });
---
+++ Assets/Scripts/Minigame/GreenColor/TongleFrog.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TongleFrog : MonoBehaviour
+{
+    [SerializeField] private RectTransform m_RectTransform;
+
+    [SerializeField] private Image image;
+
+    public float lengthTongleConst = 570f;
+
+    [SerializeField] private Transform constPosDis;
+
+    private float distanceConst;
+
+    private void Start()
+    {
+        distanceConst = Vector2.Distance(constPosDis.position, m_RectTransform.position);
+        m_RectTransform.sizeDelta = new Vector2(0, m_RectTransform.sizeDelta.y);
+        image.fillAmount = 0;
+    }
+
+    public void Eat(AnimalFlyRandom animal, Action<bool> callBack)
+    {
+        image.fillAmount = 0;
+        animal.CancelFly();
+        transform.rotation = Vector2Extensions.LookAt2D(transform.position, animal.transform.position);
+        float distance = Vector2.Distance(animal.transform.position, transform.position);
+        m_RectTransform.sizeDelta = new Vector2(570f * distance / distanceConst, m_RectTransform.sizeDelta.y);
+        image.DOFillAmount(1, 0.75f).OnComplete(() => {
+            animal.transform.parent = transform;
+            animal.GetComponent<RectTransform>().DOAnchorPosX(0, 0.75f).OnComplete(() => {
+                animal.gameObject.SetActive(false);
+            });
+            image.DOFillAmount(0, 0.75f).OnComplete(() => {
+                callBack?.Invoke(animal.isCurrentAnimal);
+            });
+        });
+    }
+}
+
+public static class Vector2Extensions
+{
+    public static Quaternion LookAt2D(this Vector2 fromPosition, Vector2 toPosition)
+    {
+        Vector2 direction = toPosition - fromPosition;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        return Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
