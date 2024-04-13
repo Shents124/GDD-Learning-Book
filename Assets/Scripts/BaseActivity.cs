08f042e111250466b413071e635b6efc7884d77d
sonnd <sonnd-han185@zitga.com.vn>
2024-04-13 09:29:40 +0700

    Thêm logic back, next

--- Assets/Scripts/BaseActivity.cs
+++ Assets/Scripts/BaseActivity.cs
@@ -10,7 +10,20 @@ public class BaseActivity : Activity
 {
     [SerializeField] protected Button backBtn;
     [SerializeField] protected Button nextBtn;
+    [SerializeField] protected ActivityType nextActivity;
 
+    protected override void Awake()
+    {
+        backBtn.onClick.AddListener(() => {
+            UIService.PlayFadeIn(() => {
+                OnClickedBackBtn().Forget();
+            });
+        });
+        
+        nextBtn.onClick.AddListener(OnClickedNextBtn);
+        
+        base.Awake();
+    }
 
     public override UniTask Initialize(Memory<object> args)
     {
@@ -22,6 +35,17 @@ public class BaseActivity : Activity
     protected virtual void InitializeData(Memory<object> args)
     {
         
+    }
+
+    private static async UniTask OnClickedBackBtn()
+    {
+        await UIService.OpenActivityAsync(ActivityType.MenuScreen);
+        UIService.PlayFadeOut();
+    }
+
+    protected virtual void OnClickedNextBtn()
+    {
+        UIService.OpenActivityWithFadeIn(nextActivity);
     }
     
     private static void FadeOut()
