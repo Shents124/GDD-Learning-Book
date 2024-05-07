77744e75c183c8785141a81ede4aaac2d7097ebe
ToanPD <toanpd@zitga.com.vn>
2024-05-07 22:39:41 +0700

    thay ảnh màu xanh dương

--- Assets/Scripts/Sound/Service/AudioUtility.cs
+++ Assets/Scripts/Sound/Service/AudioUtility.cs
@@ -12,11 +12,11 @@ namespace Sound.Service
             return LoadResourceService.LoadAsset<AudioClip>(AudioClipName.ToString());
         }
 
-        public static async void PlaySFX(this MonoBehaviour monoBehaviour, AudioClipName AudioClipName, Action callback = null)
+        public static async void PlaySFX(this MonoBehaviour monoBehaviour, AudioClipName AudioClipName, float timeDelayNext = 0, Action callback = null)
         {
             var audio = GetAudioClip(AudioClipName);
             EazySoundManager.Instance.PlaySound(audio, false);
-            await AsyncService.Delay(audio.length, monoBehaviour);
+            await AsyncService.Delay(audio.length + timeDelayNext, monoBehaviour);
             callback?.Invoke();
         }
 
